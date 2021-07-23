3e262a9f92b0a1e663f8cded407d17700dfe5c00
r0 <boogieLing_o@163.com>
2021-07-23 15:54:43 +0800

    尝试给add()增加过期时间，通过pipe作用

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as fp:
 
 setup(
     name='redis-helper',
-    version='0.4.1',
+    version='0.4.2',
     description='Easily store, index, and modify Python dicts in Redis (with flexible searching)',
     long_description=long_description,
     author='r0',
